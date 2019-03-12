a421160361f01c98d34499673395075512b61c75
remnez <remi4love2003@gmail.com>
2019-03-12 13:49:56 +0100

    styling

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,19 +13,6 @@ class PagesController extends Controller
 
     	$portfolios = Portfolio::get();
 
-  //   	foreach ($portfolios as $portfolio) {
-    	
-	 // 		$screen_shot_json_data = file_get_contents("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=".$portfolio->link."&screenshot=true");
-		// 	$screen_shot_result = json_decode($screen_shot_json_data, true);
-		// 	$screen_shot = $screen_shot_result['screenshot']['data'];
-		// 	$screen_shot = str_replace(array('_','-'), array('/', '+'), $screen_shot);
-		// 	$screen_shot_image = "<img src=\"data:image/jpeg;base64,".$screen_shot."\" class='img-responsive img-thumbnail'/>";
-		// }
-    	
-    	// return view('pages.home')->with([
-    	// 	'portfolio' => $portfolio,
-    	// 	'screen_shot' => $screen_shot_image]);
-
     	return view('pages.home')->with([
     		'portfolios' => $portfolios,]);
     }
